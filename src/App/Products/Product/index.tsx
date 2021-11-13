882bbb1a27e6f30d7fcdd1b54a6245c7d8383427
edo-edo <ediberidze99@gmail.com>
2021-11-14 01:07:01 +0400

    add product component

--- src/App/Products/Product/index.tsx
+++ src/App/Products/Product/index.tsx
@@ -1,5 +1,32 @@
 import { FC } from 'react';
 
-const Product: FC = () => <h1>product</h1>;
+interface ProductProps {
+  imageSrc: string;
+  imageAlt: string;
+  href: string;
+  name: string;
+  color: string;
+  price: string;
+}
+
+const Product: FC<ProductProps> = ({ imageSrc, imageAlt, href, name, color, price }) => (
+  <div className="group relative">
+    <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+      <img src={imageSrc} alt={imageAlt} className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
+    </div>
+    <div className="mt-4 flex justify-between">
+      <div>
+        <h3 className="text-sm text-gray-700">
+          <a href={href}>
+            <span aria-hidden="true" className="absolute inset-0" />
+            {name}
+          </a>
+        </h3>
+        <p className="mt-1 text-sm text-gray-500">{color}</p>
+      </div>
+      <p className="text-sm font-medium text-gray-900">{price}</p>
+    </div>
+  </div>
+);
 
 export default Product;
--- src/App/Products/index.tsx
+++ src/App/Products/index.tsx
@@ -1,43 +1,7 @@
 import { FC } from 'react';
 
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: 2,
-    name: 'Basic bol',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$45',
-    color: 'Black',
-  },
-  {
-    id: 3,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: 4,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-];
+import Product from './Product';
+import { products } from '../data/productList.json';
 
 const Products: FC = () => (
   <div className="bg-white">
@@ -46,27 +10,15 @@ const Products: FC = () => (
 
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {products.map((product) => (
-          <div key={product.id} className="group relative">
-            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-              <img
-                src={product.imageSrc}
-                alt={product.imageAlt}
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href={product.href}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {product.name}
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">{product.price}</p>
-            </div>
-          </div>
+          <Product
+            key={product.id}
+            imageSrc={product.imageSrc}
+            imageAlt={product.imageAlt}
+            href={product.href}
+            name={product.name}
+            color={product.color}
+            price={product.price}
+          />
         ))}
       </div>
     </div>
