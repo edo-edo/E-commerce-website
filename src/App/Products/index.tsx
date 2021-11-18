61ec99998b8a1674de52478323d9dec0c73402f8
edo-edo <ediberidze99@gmail.com>
2021-11-18 23:43:09 +0400

    Add navigation component

--- src/App/Products/index.tsx
+++ src/App/Products/index.tsx
@@ -3,12 +3,12 @@ import { FC } from 'react';
 import Product from './Product';
 import Pagination from '../Common/Pagination';
 import { products } from '../data/productList.json';
+import Navigation from '../Common/Navigation';
 
 const Products: FC = () => (
   <div className="bg-white">
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
-
+    <Navigation />
+    <div className="max-w-2xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {products.map((product) => (
           <Product
