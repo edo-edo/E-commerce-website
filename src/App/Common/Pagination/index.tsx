8d801b950e95520e878cecc02911fe39b7128592
edo-edo <ediberidze99@gmail.com>
2021-11-22 23:19:30 +0400

    Add pagination number component

--- src/App/Common/Pagination/index.tsx
+++ src/App/Common/Pagination/index.tsx
@@ -1,49 +1,19 @@
 import { FC } from 'react';
 
+import Number from './Number';
+
 const Pagination: FC = () => (
   <nav className="border-t border-gray-200 px-4 mt-24 flex  justify-center sm:px-0">
     <div className="md:-mt-px flex">
-      <a
-        href="#"
-        className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 flex items-center text-sm font-medium"
-      >
-        1
-      </a>
-      {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
-      <a
-        href="#"
-        className="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 flex items-center text-sm font-medium"
-        aria-current="page"
-      >
-        2
-      </a>
-      <a
-        href="#"
-        className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 flex items-center text-sm font-medium"
-      >
-        3
-      </a>
+      <Number num={1} />
+      <Number num={2} active={true} />
+      <Number num={3} />
       <span className="border-transparent text-gray-500 border-t-2 pt-4 px-4 flex items-center text-sm font-medium">
         ...
       </span>
-      <a
-        href="#"
-        className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 flex items-center text-sm font-medium"
-      >
-        8
-      </a>
-      <a
-        href="#"
-        className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 flex items-center text-sm font-medium"
-      >
-        9
-      </a>
-      <a
-        href="#"
-        className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 flex items-center text-sm font-medium"
-      >
-        10
-      </a>
+      <Number num={8} />
+      <Number num={9} />
+      <Number num={10} />
     </div>
   </nav>
 );
